aa2ec37f1d1a232c0f8e5de5e72f8f88dfe4015e
fps <mista.tapas@gmx.net>
2021-10-16 22:36:20 +0200

    fix whitespace

--- ogfx_jack_switch.cc
+++ ogfx_jack_switch.cc
@@ -10,7 +10,7 @@ int channel_switch = 0;
 int quit = 0;
 
 static void signal_handler(int signo) {
-  quit = 1;
+    quit = 1;
 }
 
 jack_port_t *in0;
@@ -22,103 +22,105 @@ jack_port_t *out10;
 jack_port_t *out11;
 
 extern "C" {
-  int process(jack_nframes_t nframes, void *arg) {
-    void *in0_buffer = jack_port_get_buffer(in0, nframes);
-    void *in1_buffer = jack_port_get_buffer(in1, nframes);
-    void *out00_buffer = jack_port_get_buffer(out00, nframes);
-    void *out01_buffer = jack_port_get_buffer(out01, nframes);
-    void *out10_buffer = jack_port_get_buffer(out10, nframes);
-    void *out11_buffer = jack_port_get_buffer(out11, nframes);
-    
-    if (0 == channel_switch) {
-      memcpy(out00_buffer, in0_buffer, nframes * sizeof(float));
-      memcpy(out01_buffer, in1_buffer, nframes * sizeof(float));
+    int process(jack_nframes_t nframes, void *arg) {
+        void *in0_buffer = jack_port_get_buffer(in0, nframes);
+        void *in1_buffer = jack_port_get_buffer(in1, nframes);
+        void *out00_buffer = jack_port_get_buffer(out00, nframes);
+        void *out01_buffer = jack_port_get_buffer(out01, nframes);
+        void *out10_buffer = jack_port_get_buffer(out10, nframes);
+        void *out11_buffer = jack_port_get_buffer(out11, nframes);
 
-      memset(out10_buffer, 0, nframes * sizeof(float));
-      memset(out11_buffer, 0, nframes * sizeof(float));
-    } else {
-      memset(out00_buffer, 0, nframes * sizeof(float));
-      memset(out01_buffer, 0, nframes * sizeof(float));
+        if (0 == channel_switch) {
+            memcpy(out00_buffer, in0_buffer, nframes * sizeof(float));
+            memcpy(out01_buffer, in1_buffer, nframes * sizeof(float));
 
-      memcpy(out10_buffer, in0_buffer, nframes * sizeof(float));
-      memcpy(out11_buffer, in1_buffer, nframes * sizeof(float));
+            memset(out10_buffer, 0, nframes * sizeof(float));
+            memset(out11_buffer, 0, nframes * sizeof(float));
+        } else {
+            memset(out00_buffer, 0, nframes * sizeof(float));
+            memset(out01_buffer, 0, nframes * sizeof(float));
+
+            memcpy(out10_buffer, in0_buffer, nframes * sizeof(float));
+            memcpy(out11_buffer, in1_buffer, nframes * sizeof(float));
+        }
+        return 0;
     }
-    return 0;
-  }
 }
 
 /*
-  A small program that switches the stereo input signal to either
-  of two output channel pairs. Which output pair to switch to 
-  depends on the input to the process' stdin: a "0" routes the 
-  signal to outputs 00 and 01 and a "1" routes the signal to 
-  outputs 10 and 11.
+    A small program that switches the stereo input signal to either
+    of two output channel pairs. Which output pair to switch to
+    depends on the input to the process' stdin: a "0" routes the
+    signal to outputs 00 and 01 and a "1" routes the signal to
+    outputs 10 and 11.
 */
 int main(int argc, char *argv[]) {
-  signal(SIGINT, signal_handler);
+    signal(SIGINT, signal_handler);
 
-  std::string name;
-  
-  namespace po = boost::program_options;
-  po::options_description desc("Allowed options");
-  desc.add_options()
-    ("help,h",
-     "get some help")
-    ("name,n",
-     po::value<std::string>(&name)->default_value("jack_switch"),
-     "the jack client name");
+    std::string name;
 
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
+    namespace po = boost::program_options;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h",
+         "get some help")
+        ("name,n",
+         po::value<std::string>(&name)->default_value("jack_switch"),
+         "the jack client name");
 
-  if (vm.count("help")) {
-    std::cout << desc << std::endl;
-    return EXIT_SUCCESS;
-  }
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
 
-  
-  jack_status_t jack_status;
-  jack_client_t *jack_client = jack_client_open(name.c_str(), JackNullOption, &jack_status);
-
-  if (NULL == jack_client) {
-    std::cerr << "ogfx_jack_switch: Failed to create jack client. Exiting..." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  in0 = jack_port_register(jack_client, "in0", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-  in1 = jack_port_register(jack_client, "in1", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-  
-  out00 = jack_port_register(jack_client, "out00", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-  out01 = jack_port_register(jack_client, "out01", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-  
-  out10 = jack_port_register(jack_client, "out10", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-  out11 = jack_port_register(jack_client, "out11", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-
-  if (0 != jack_set_process_callback(jack_client, process, 0)) {
-    std::cerr << "ogfx_jack_switch: Failed to set process callback. Exiting..." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  if (0 != jack_activate(jack_client)) {
-    std::cerr << "ogfx_jack_switch: Failed to activate. Exiting..." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  std::cout << "ogfx_jack_switch: 0CR for output channels 00/01, 1CR for output channels 10/11. EOF to quit" << std::endl;
-  
-  int n;
-  while (quit == 0 && std::cin >> n) {
-    std::cout << "ogfx_jack_switch: got : " << n << std::endl;
-    if (0 == n) {
-      channel_switch = 0;
-    } else {
-      channel_switch = 1;
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return EXIT_SUCCESS;
     }
-  }
 
-  std::cout << "ogfx_jack_switch: exiting..." << std::endl;
-  
-  jack_deactivate(jack_client);
-  jack_client_close(jack_client);
+
+    jack_status_t jack_status;
+    jack_client_t *jack_client = jack_client_open(name.c_str(), JackNullOption, &jack_status);
+
+    if (NULL == jack_client) {
+        std::cerr << "ogfx_jack_switch: Failed to create jack client. Exiting..." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    in0 = jack_port_register(jack_client, "in0", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+    in1 = jack_port_register(jack_client, "in1", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+
+    out00 = jack_port_register(jack_client, "out00", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+    out01 = jack_port_register(jack_client, "out01", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+
+    out10 = jack_port_register(jack_client, "out10", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+    out11 = jack_port_register(jack_client, "out11", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+
+    if (0 != jack_set_process_callback(jack_client, process, 0)) {
+        std::cerr << "ogfx_jack_switch: Failed to set process callback. Exiting..." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (0 != jack_activate(jack_client)) {
+        std::cerr << "ogfx_jack_switch: Failed to activate. Exiting..." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "ogfx_jack_switch: 0CR for output channels 00/01, 1CR for output channels 10/11. EOF to quit" << std::endl;
+
+    int n;
+    while (quit == 0 && std::cin >> n) {
+        std::cout << "ogfx_jack_switch: got : " << n << std::endl;
+        if (0 == n) {
+            channel_switch = 0;
+        } else {
+            channel_switch = 1;
+        }
+    }
+
+    std::cout << "ogfx_jack_switch: exiting..." << std::endl;
+
+    jack_deactivate(jack_client);
+    jack_client_close(jack_client);
+
+    return EXIT_SUCCESS;
 }
